826e23907545839b0f9ee00ae5ff6abf1cdbd237
Jannik <jannik44@hotmail.de>
2023-05-03 14:23:05 +0200

    changed some more stuff

--- service/CVExchange/routes/comments.js
+++ service/CVExchange/routes/comments.js
@@ -3,12 +3,37 @@ const router = express.Router()
 const jwt = require('jsonwebtoken')
 const auth_middleware = require('../middleware/auth')
 const auth = auth_middleware.auth
-const jwtSecret = auth_middleware.jwtSecret
 
 
 //define all of the routes
 
-router.post('/new' , auth, (req, res) => {
+router.post('/new' , auth, getusername, (req, res) => {
+    const comment = req.body.comment
+    const postId = req.body.postId
+    const creatorId = req.userId
+    const creatorName = req.username
+
+    const query = `INSERT INTO comments (text, post_id, creator_id, creator_name,  rating, datetime) VALUES ('${comment}', '${postId}', '${creatorId}', '${creatorName}',  '1',  NOW() )`
+    req.database.query(query, (error, results) => {
+        if(error) throw error
+        res.redirect(`/posts/${postId}`)
+    })
+})
+
+//TODO: add functionality to these routes 
+router.post('/edit' , auth, (req, res) => {
+    const comment = req.body.comment
+    const postId = req.body.postId
+    const creatorId = req.userId
+
+    const query = `INSERT INTO comments (text, post_id, creator_id, rating, datetime) VALUES ('${comment}', '${postId}', '${creatorId}', '1',  NOW() )`
+    req.database.query(query, (error, results) => {
+        if(error) throw error
+        res.redirect(`/posts/${postId}`)
+    })
+})
+
+router.post('/delete' , auth, (req, res) => {
     const comment = req.body.comment
     const postId = req.body.postId
     const creatorId = req.userId
@@ -22,6 +47,19 @@ router.post('/new' , auth, (req, res) => {
 
 //--------------------------------
 
+//define middleware
 
+//middleware to get the username for the request
+function getusername(req, res, next) {
+    const userId = req.userId
+    const query = `SELECT * FROM users WHERE id = ${userId}`
+    req.database.query(query, (error, results) => {
+        if(error) throw error
+        req.username = results[0].name
+        next()
+    })
+}
+
+//--------------------------------
 
 module.exports = router
--- service/CVExchange/routes/posts.js
+++ service/CVExchange/routes/posts.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const jwt = require('jsonwebtoken')
 const auth_middleware = require('../middleware/auth')
 const auth = auth_middleware.auth
-const jwtSecret = auth_middleware.jwtSecret
 
 //define all of the routes
 router.get('/', (req, res) => {
@@ -23,12 +22,13 @@ router.get('/new', auth,  (req, res) => {
     res.render('newpost')
 })
 
-router.post('/new', auth, (req, res) => {
+router.post('/new', auth, getusername,  (req, res) => {
     const title = req.body.title
     const text = req.body.text
     const creatorId = req.userId
+    const creatorName = req.username
 
-    const query = `INSERT INTO posts (title, text, rating, creator_id, datetime) VALUES ('${title}', '${text}', '1', '${creatorId}', NOW() )`
+    const query = `INSERT INTO posts (title, text, rating, creator_id, creator_name, datetime) VALUES ('${title}', '${text}', '1', '${creatorId}', '${creatorName}', NOW() )`
     req.database.query(query, (error, results) => {
         if(error) throw error
         res.status(200).send('Post created successfully')
@@ -71,7 +71,7 @@ router.post('/delete/:id', auth,  (req, res) => {
             const delete_query = `DELETE FROM posts WHERE id = ${postId}`
             req.database.query(delete_query, (error, results) => {
                 if(error) throw error
-                res.status(200).send('Post deleted successfully!')
+                res.redirect('/users/myposts')
             })
         }
         else {
@@ -105,7 +105,7 @@ router.post('/edit/:id', auth,  (req, res) => {
     const query = `UPDATE posts SET title = '${title}', text = '${text}' WHERE id = ${postId} AND creator_id = ${userId}`
     req.database.query(query, (error, results) => {
         if(error) throw error
-        res.status(200).send('Post edited successfully!')
+        res.redirect('/users/myposts')
     })
 })
 
@@ -114,11 +114,16 @@ router.post('/edit/:id', auth,  (req, res) => {
 
 //define middleware
 
-//don't really use this one (might use later)
-router.param('postId', (req, res, next, postId) => {
-    req.post = posts[postId]
-    next()
-})
+//middleware to get the username for the request
+function getusername(req, res, next) {
+    const userId = req.userId
+    const query = `SELECT * FROM users WHERE id = ${userId}`
+    req.database.query(query, (error, results) => {
+        if(error) throw error
+        req.username = results[0].name
+        next()
+    })
+}
 
 //--------------------------------
 
