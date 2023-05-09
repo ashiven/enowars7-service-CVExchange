77036dce772b3e0c8906cf09b8f2214fb396b972
Jannik <jannik44@hotmail.de>
2023-05-09 22:06:20 +0200

    improved the comment/post rating endpoint

--- service/CVExchange/routes/votes.js
+++ service/CVExchange/routes/votes.js
@@ -26,11 +26,20 @@ router.post('/ratepost', auth, async (req, res) => {
         const search_params = [userId, postId]
         const [searchResults] = await connection.query(search_query, search_params)
 
-        //if the user already voted on that post, update their rating
+        //if the user already voted on that post, update/delete their rating
         if (searchResults.length > 0) {
-            const update_query = `UPDATE ratings SET rating = ? WHERE user_id = ? AND post_id = ?`
-            const update_params = [rating, userId, postId]
-            await connection.query(update_query, update_params)
+            //they are submitting the same rating again
+            if(searchResults[0].rating === rating) {
+                const delete_query = `DELETE FROM ratings WHERE user_id = ? AND post_id = ?`
+                const delete_params = [userId, postId]
+                await connection.query(delete_query, delete_params)
+            }
+            //they are submitting the opposite rating
+            else {
+                const update_query = `UPDATE ratings SET rating = ? WHERE user_id = ? AND post_id = ?`
+                const update_params = [rating, userId, postId]
+                await connection.query(update_query, update_params)
+            }
         }
         //if they haven't voted, insert a new rating into the DB
         else {
@@ -42,12 +51,20 @@ router.post('/ratepost', auth, async (req, res) => {
         //now we accumulate the ratings of a post and update the rating entry for it
         const acc_query = `SELECT SUM(rating) AS total_rating FROM ratings WHERE post_id = ?`
         const acc_params = [postId]
-        const [acc_results] = await connection.query(acc_query, acc_params)
+        const [accResults] = await connection.query(acc_query, acc_params)
 
-        const total = acc_results[0].total_rating
-        const post_query = `UPDATE posts SET rating = ? WHERE id = ?`
-        const post_params = [total, postId]
-        await connection.query(post_query, post_params)
+        //we need an if/else in case the post has 0 votes, meaning it should receive a rating of 0
+        const total = accResults[0].total_rating
+        if(total !== null) {
+            const post_query = `UPDATE posts SET rating = ? WHERE id = ?`
+            const post_params = [total, postId]
+            await connection.query(post_query, post_params)
+        }
+        else {
+            const post_query = `UPDATE posts SET rating = 0 WHERE id = ?`
+            const post_params = [postId]
+            await connection.query(post_query, post_params)
+        }
 
         // commit the transaction and release the connection
         await connection.commit()
@@ -85,11 +102,20 @@ router.post('/ratecomment', auth, async (req, res) => {
         const search_params = [userId, commentId]
         const [searchResults] = await connection.query(search_query, search_params)
 
-        //if the user already voted on that comment, update their rating
-        if(searchResults.length > 0) {
-            const update_query = `UPDATE ratings SET rating = ? WHERE user_id = ? AND comment_id = ?`
-            const update_params = [rating, userId, commentId]
-            await connection.query(update_query, update_params)
+        //if the user already voted on that comment, update/delete their rating
+        if (searchResults.length > 0) {
+            //they are submitting the same rating again
+            if(searchResults[0].rating === rating) {
+                const delete_query = `DELETE FROM ratings WHERE user_id = ? AND comment_id = ?`
+                const delete_params = [userId, commentId]
+                await connection.query(delete_query, delete_params)
+            }
+            //they are submitting the opposite rating
+            else {
+                const update_query = `UPDATE ratings SET rating = ? WHERE user_id = ? AND comment_id = ?`
+                const update_params = [rating, userId, commentId]
+                await connection.query(update_query, update_params)
+            }
         }
 
         //if they haven't voted, insert a new rating into the DB
@@ -104,10 +130,18 @@ router.post('/ratecomment', auth, async (req, res) => {
         const acc_params = [commentId]
         const [accResults] = await connection.query(acc_query, acc_params)
 
+        //we need an if/else in case the comment has 0 votes, meaning it should receive a rating of 0
         const total = accResults[0].total_rating
-        const comment_query = `UPDATE comments SET rating = ? WHERE id = ?`
-        const comment_params = [total, commentId]
-        await connection.query(comment_query, comment_params)
+        if(total !== null) {
+            const comment_query = `UPDATE comments SET rating = ? WHERE id = ?`
+            const comment_params = [total, commentId]
+            await connection.query(comment_query, comment_params)
+        }
+        else {
+            const comment_query = `UPDATE comments SET rating = 0 WHERE id = ?`
+            const comment_params = [commentId]
+            await connection.query(comment_query, comment_params)
+        }
 
         // commit the transaction and release the connection
         await connection.commit()
@@ -120,33 +154,6 @@ router.post('/ratecomment', auth, async (req, res) => {
         await connection.rollback()
         await connection.release()
         
-        console.error(error)
-        return res.status(500).send('<h1>Internal Server Error</h1>')
-    }
-})
-
-router.post('/delete/:id', auth, async (req, res) => {
-    try {
-        const ratingId = req.params.id
-        const userId = req.userId
-        const page = req.body.page
-
-        const find_query = `SELECT * FROM ratings WHERE id = ? AND user_id = ?`
-        const find_params = [ratingId, userId]
-        const [find_results] = await req.database.query(find_query, find_params)
-
-        if (find_results.length > 0) {
-            const delete_query = `DELETE FROM ratings WHERE id = ?`
-            const delete_params = [ratingId]
-            await req.database.query(delete_query, delete_params)
-
-            return res.redirect(`${page}`)
-        } 
-        else {
-            return res.status(401).send('You are not authorized to delete this rating or it doesnt exist')
-        }
-    } 
-    catch (error) {
         console.error(error)
         return res.status(500).send('<h1>Internal Server Error</h1>')
     }
