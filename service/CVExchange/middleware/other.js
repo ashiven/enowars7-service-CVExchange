8228b154e1bec27006bd0f265878200ce63b88fb
Ashiven <rodgiven@outlook.de>
2023-07-04 01:13:25 +0200

    implemented cache for middleware

--- service/CVExchange/middleware/other.js
+++ service/CVExchange/middleware/other.js
@@ -16,8 +16,16 @@ async function getusername(req, res, next) {
          const userId = req.userId
          const query = "SELECT * FROM users WHERE id = ?"
          const params = [userId]
-         const [results] = await req.database.query(query, params)
-         req.username = results[0].name
+
+         hit = req.cache.get(req.userId + "UN")
+
+         if (hit == undefined) {
+            const [results] = await req.database.query(query, params)
+            req.username = results[0].name
+            req.cache.set(req.userId + "UN", { username: results[0].name }, 100)
+         } else {
+            req.username = hit.username
+         }
       }
       next()
    } catch (error) {
@@ -32,8 +40,15 @@ async function getuserratings(req, res, next) {
          const userId = req.userId
          const query = "SELECT * FROM ratings WHERE user_id = ?"
          const params = [userId]
-         const [results] = await req.database.query(query, params)
-         req.ratings = results
+         hit = req.cache.get(req.userId + "UR")
+
+         if (hit == undefined) {
+            const [results] = await req.database.query(query, params)
+            req.ratings = results
+            req.cache.set(req.userId + "UR", { ratings: results }, 100)
+         } else {
+            req.ratings = hit.ratings
+         }
       }
       next()
    } catch (error) {
@@ -63,22 +78,37 @@ async function getuserkarma(req, res, next) {
       if (req.userId) {
          const userId = req.userId
 
-         const postQuery = "SELECT * FROM posts WHERE creator_id = ?"
-         const postParams = [userId]
-         const [posts] = await req.database.query(postQuery, postParams)
+         hit = req.cache.get(req.userId + "UK")
 
-         const commentQuery = "SELECT * FROM comments WHERE creator_id = ?"
-         const commentParams = [userId]
-         const [comments] = await req.database.query(
-            commentQuery,
-            commentParams
-         )
+         if (hit == undefined) {
+            const postQuery = "SELECT * FROM posts WHERE creator_id = ?"
+            const postParams = [userId]
+            const [posts] = await req.database.query(postQuery, postParams)
 
-         req.postkarma = posts.reduce((total, post) => total + post.rating, 0)
-         req.commentkarma = comments.reduce(
-            (total, comment) => total + comment.rating,
-            0
-         )
+            const commentQuery = "SELECT * FROM comments WHERE creator_id = ?"
+            const commentParams = [userId]
+            const [comments] = await req.database.query(
+               commentQuery,
+               commentParams
+            )
+
+            req.postkarma = posts.reduce(
+               (total, post) => total + post.rating,
+               0
+            )
+            req.commentkarma = comments.reduce(
+               (total, comment) => total + comment.rating,
+               0
+            )
+            req.cache.set(
+               req.userId + "UK",
+               { postkarma: req.postkarma, commentkarma: req.commentkarma },
+               100
+            )
+         } else {
+            req.commentkarma = hit.commentkarma
+            req.postkarma = hit.postkarma
+         }
       }
       next()
    } catch (error) {
@@ -92,18 +122,25 @@ async function getsubids(req, res, next) {
       if (req.userId) {
          const userId = req.userId
 
-         const selectQuery = "SELECT subscribed FROM users WHERE id = ?"
-         const selectParams = [userId]
-         const [subscribedRes] = await req.database.query(
-            selectQuery,
-            selectParams
-         )
-         const subbedString = subscribedRes[0].subscribed
-         const subscribed = subbedString
-            ? subbedString.split(",").map(Number)
-            : []
+         hit = req.cache.get(req.userId + "SI")
 
-         req.subscribed = subscribed
+         if (hit == undefined) {
+            const selectQuery = "SELECT subscribed FROM users WHERE id = ?"
+            const selectParams = [userId]
+            const [subscribedRes] = await req.database.query(
+               selectQuery,
+               selectParams
+            )
+            const subbedString = subscribedRes[0].subscribed
+            const subscribed = subbedString
+               ? subbedString.split(",").map(Number)
+               : []
+
+            req.subscribed = subscribed
+            req.cache.set(req.userId + "SI", { subscribed: subscribed }, 100)
+         } else {
+            req.subscribed = hit.subscribed
+         }
       }
       next()
    } catch (error) {
@@ -115,11 +152,17 @@ async function getsubids(req, res, next) {
 async function getsubs(req, res, next) {
    try {
       if (req.userId && req.subscribed.length > 0) {
-         const selectQuery = "SELECT * FROM subs WHERE id IN (?)"
-         const selectParams = [req.subscribed]
-         const [subs] = await req.database.query(selectQuery, selectParams)
+         hit = req.cache.get(req.userId + "S")
+         if (hit == undefined) {
+            const selectQuery = "SELECT * FROM subs WHERE id IN (?)"
+            const selectParams = [req.subscribed]
+            const [subs] = await req.database.query(selectQuery, selectParams)
 
-         req.subs = subs
+            req.subs = subs
+            req.cache.set(req.userId + "S", { subs: subs }, 100)
+         } else {
+            req.subs = hit.subs
+         }
       }
       next()
    } catch (error) {
@@ -130,11 +173,17 @@ async function getsubs(req, res, next) {
 
 async function gettopsubs(req, res, next) {
    try {
-      const selectQuery = "SELECT * FROM subs ORDER BY members DESC LIMIT 17"
-      const selectParams = [req.subscribed]
-      const [subs] = await req.database.query(selectQuery, selectParams)
+      hit = req.cache.get(req.userId + "TS")
+      if (hit == undefined) {
+         const selectQuery = "SELECT * FROM subs ORDER BY members DESC LIMIT 17"
+         const selectParams = [req.subscribed]
+         const [subs] = await req.database.query(selectQuery, selectParams)
 
-      req.topsubs = subs
+         req.topsubs = subs
+         req.cache.set(req.userId + "TS", { topsubs: subs }, 100)
+      } else {
+         req.topsubs = hit.topsubs
+      }
       next()
    } catch (error) {
       console.error(error)
