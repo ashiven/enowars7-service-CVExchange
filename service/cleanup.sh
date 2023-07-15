bdbaa230971c0a94d78e4544661fcd86a6b73484
Ashiven <rodgiven@outlook.de>
2023-07-15 02:50:25 +0200

    fix cleanup script

--- service/cleanup.sh
+++ service/cleanup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-while true; do
-    mysql -psecret -e"USE basedbase; DELETE FROM users WHERE datetime <= DATE_SUB(NOW(), INTERVAL 30 MINUTE);"
-    mysql -psecret -e"USE basedbase; DELETE FROM subs WHERE datetime <= DATE_SUB(NOW(), INTERVAL 30 MINUTE);"
-    mysql -psecret -e"USE basedbase; DELETE FROM posts WHERE datetime <= DATE_SUB(NOW(), INTERVAL 30 MINUTE);"
-    mysql -psecret -e"USE basedbase; DELETE FROM comments WHERE datetime <= DATE_SUB(NOW(), INTERVAL 30 MINUTE);"
-    mysql -psecret -e"USE basedbase; DELETE FROM ratings WHERE datetime <= DATE_SUB(NOW(), INTERVAL 30 MINUTE);"
+while : 
+do
+    echo "Cleaning up users"
+    mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -e"USE $MYSQL_DB; DELETE FROM users WHERE datetime <= DATE_SUB(NOW(), INTERVAL 30 MINUTE);"
+    echo "Cleaning up subexchanges"
+    mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -e"USE $MYSQL_DB; DELETE FROM subs WHERE datetime <= DATE_SUB(NOW(), INTERVAL 30 MINUTE);"
+    echo "Cleaning up posts"
+    mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -e"USE $MYSQL_DB; DELETE FROM posts WHERE datetime <= DATE_SUB(NOW(), INTERVAL 30 MINUTE);"
+    echo "Cleaning up comments"
+    mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -e"USE $MYSQL_DB; DELETE FROM comments WHERE datetime <= DATE_SUB(NOW(), INTERVAL 30 MINUTE);"
+    echo "Cleaning up ratings"
+    mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -e"USE $MYSQL_DB; DELETE FROM ratings WHERE datetime <= DATE_SUB(NOW(), INTERVAL 30 MINUTE);"
     sleep 1m
 done
