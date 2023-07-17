6dc27e805c6de4d5b7de97347c9e701d52bceafd
Ashiven <rodgiven@outlook.de>
2023-07-17 14:11:32 +0200

    Update cleanup script to delete records older than 10 minutes

--- service/cleanup.sh
+++ service/cleanup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "Cleaning up backup directory"
-find "/app/backups/" -mindepth 1 -mmin +30 -delete
+find "/app/backups/" -mindepth 1 -mmin +10 -delete
 echo "Cleaning up uploads directory"
-find "/app/uploads/" -mindepth 1 -mmin +30 -delete
+find "/app/uploads/" -mindepth 1 -mmin +10 -delete
 
 echo "Cleaning up users"
-mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -e"USE $MYSQL_DB; DELETE FROM users WHERE datetime <= DATE_SUB(NOW(), INTERVAL 30 MINUTE);"
+mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -e"USE $MYSQL_DB; DELETE FROM users WHERE datetime <= DATE_SUB(NOW(), INTERVAL 10 MINUTE);"
 echo "Cleaning up subexchanges"
-mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -e"USE $MYSQL_DB; DELETE FROM subs WHERE datetime <= DATE_SUB(NOW(), INTERVAL 30 MINUTE);"
+mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -e"USE $MYSQL_DB; DELETE FROM subs WHERE datetime <= DATE_SUB(NOW(), INTERVAL 10 MINUTE);"
 echo "Cleaning up posts"
-mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -e"USE $MYSQL_DB; DELETE FROM posts WHERE datetime <= DATE_SUB(NOW(), INTERVAL 30 MINUTE);"
+mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -e"USE $MYSQL_DB; DELETE FROM posts WHERE datetime <= DATE_SUB(NOW(), INTERVAL 10 MINUTE);"
 echo "Cleaning up comments"
-mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -e"USE $MYSQL_DB; DELETE FROM comments WHERE datetime <= DATE_SUB(NOW(), INTERVAL 30 MINUTE);"
+mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -e"USE $MYSQL_DB; DELETE FROM comments WHERE datetime <= DATE_SUB(NOW(), INTERVAL 10 MINUTE);"
 echo "Cleaning up ratings"
-mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -e"USE $MYSQL_DB; DELETE FROM ratings WHERE datetime <= DATE_SUB(NOW(), INTERVAL 30 MINUTE);"
+mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -e"USE $MYSQL_DB; DELETE FROM ratings WHERE datetime <= DATE_SUB(NOW(), INTERVAL 10 MINUTE);"
